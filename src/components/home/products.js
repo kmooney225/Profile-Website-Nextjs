033b284ba37d0435c83fbc98da426381e60cde40
kmooney225 <kmooney225@gmail.com>
2023-01-12 14:05:33 -0700

    edit fonts

--- src/components/home/products.js
+++ src/components/home/products.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Product = () =>{
     return(
         <section className="relative flex flex-col p-16 object-cover items-center justify-center bg-fixed bg-center bg-black z-[3]">
             <div className='flex flex-col md:flex-row'>
                 <div className='z-[2] p-16 rounded-md bg-red-600 items-center justify-center text-white md:w-1/2'>
-                    <h2 className='p-4 text-3xl font-bold'>
+                    <h2 className='p-4 text-6xl font-sacramento'>
                         Music
                     </h2>
                     <p className='text-md m-4'>
                         I offer services with music for different venues type. This include Weddings, resturants,
-                        and party venues
+                        and party venues.
                     </p>
                     <button className="bg-transparent hover:bg-gray-800 text-white font-semibold hover:text-white p-4 border-2 border-gray-400 hover:border-transparent rounded-lg">
-                        Check It Out!
+                        <Link href='/music'>
+                            Check It Out!
+                        </Link>
                     </button>
                 </div>
                 <div className='z-[2] bg-gray-300 rounded-md p-4 items-center justify-center md:w-1/2'>
@@ -34,14 +37,16 @@ const Product = () =>{
                     />
                 </div>
                 <div className='z-[2] p-16 rounded-md bg-red-600 items-center justify-center text-white md:w-1/2'>
-                    <h2 className='p-4 text-3xl font-bold'>
+                    <h2 className='p-4 text-6xl font-sacramento'>
                         Web Dev
                     </h2>
-                    <p className='text-md m-4'>I offer services with music for different venues type. This include Weddings, resturants,
-                        and party venues
+                    <p className='text-md m-4'>I create websites for different things, from professionals needing a digital portfolio 
+                    to small business needing a brouchure website of their product!
                     </p>
                     <button className="bg-transparent hover:bg-gray-800 text-white font-semibold hover:text-white p-4 border-2 border-gray-400 hover:border-transparent rounded-lg">
-                        Check It Out!
+                        <Link href='/music'>
+                            Check It Out!
+                        </Link>
                     </button>
                 </div>
             </div>

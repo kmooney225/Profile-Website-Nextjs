81082ab965ba817a5f1896d8e450d9193e513f8e
kmooney225 <kmooney225@gmail.com>
2023-01-16 13:43:41 -0700

    editing packages section for webdev

--- pages/webdev.js
+++ pages/webdev.js
@@ -1,7 +1,7 @@
 import Header from "../src/components/webdev/header"
 import Description from "../src/components/webdev/description"
-import Package from "../src/components/music/package"
-import Portfolio from "../src/components/music/portfolio"
+import Package from "../src/components/webdev/package"
+import Portfolio from "../src/components/webdev/portfolio"
 
 export default function Webdev() {
 

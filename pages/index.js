d58e56e879cd6c8a540d9b2e78992a03617cfbc7
kmooney225 <kmooney225@gmail.com>
2023-01-26 11:04:11 -0700

    fixing deployment error

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Hero from '../src/components/home/Hero';
+import Hero from '../src/components/home/hero';
 import Bio from '../src/components/home/bio';
 import Services from '../src/components/home/services';
 import Background from '../src/components/layouts/background';

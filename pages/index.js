6844636d8d9755f8bc79fc0de583831d66b39109
kmooney225 <kmooney225@gmail.com>
2024-03-28 15:19:32 -0600

    stuff added

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,10 @@ export default function Home() {
     <div>
       <NextSeo
             title="Kevin Mooney"
-            description="I am a professional saxophone player and web developer. Here is my digital portfolio!"
+            description="A professional saxophone player and software engineer based out of Denver, Colorado.
+            Kevin Mooney have played many type of musical venues including the Hard Rock Cafe, Ritz Carlton and Four Seasons.
+            Originally being from a town in Alabama near New Orleans, Kevin have experience with playing Jazz and RnB music
+            along with his adventures with busking."
         />
       <Hero />
       <Bio />

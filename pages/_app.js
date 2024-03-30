bef82eab206ff7fcfe6c656994c77021fdd39b18
kmooney225 <kmooney225@gmail.com>
2024-03-29 18:29:23 -0600

    adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../src/styles/index.scss'
 import Header from '../src/components/layouts/header';
 import Footer from '../src/components/layouts/footer';
+import Script from "next/script";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
@@ -8,6 +9,19 @@ import "slick-carousel/slick/slick-theme.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+         `}
+      </Script>
       <Header />
       <Component {...pageProps} />
       <Footer />
